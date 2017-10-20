0c793d57e594a36209061cb06e626e9ec6bb80be
sunyAAA <luna_4ever@qq.com>
2017-10-20 22:00:13 +0800

    fix dealy time

--- Slide.vue
+++ Slide.vue
@@ -32,154 +32,155 @@
 </template>
 
 <script type='text/ecmascript-6'>
-    export default{
-        props: {
-            slides: {
-                type: Array,
-                default: []
-            },
-            inv: {
-                type: Number,
-                default: 1000
-            }
-        },
-        data(){
-            return {
-                imgWidth:900,
-                nowIndex: 0,
-                invId: null,
-                isShow: true,
-                slideReset: false,
-                throttleId:null,
-                delay:1000
-            }
-        },
-        computed: {
-            slideIndex(){
-                if (this.nowIndex <= -1) {
-                    return this.slides.length - 1;
-                } else if (this.nowIndex >= this.slides.length) {
-                    return 0
-                } else {
-                    return this.nowIndex
-                }
-            }
-        },
-        methods: {
-            prev(){
-                this.nowIndex--
-            },
-            next(){
-                this.nowIndex++
-            },
-            goTo(index){
-                this.nowIndex = index;
-            },
-            throttle(method,arg){
-                if(this.throttleId){
-                    return
-                }
-                method(arg)
-                this.throttleId=setTimeout(()=>{
-                    this.throttleId=undefined;
-                },this.delay);
-            },
-            runInv(){
-               this.invId =  setInterval(()=>{
-                           this.nowIndex++
-                       },this.inv)
-            },
-            clearInv(){
-                clearInterval(this.invId);
-                this.invId=null;
-            },
-
-        },
-        watch: {
-            nowIndex(curVal){
-                if(curVal>this.slides.length || curVal<-1){
-                    this.slideReset = true;
-                    if(curVal>this.slides.length){
-                        this.nowIndex=0;
-                    }else if(curVal<-1){
-                        this.nowIndex=this.slides.length-1;
-                    }
-                    setTimeout(function(){
-                        this.slideReset = false;
-                        if(curVal>this.slides.length){
-                            this.nowIndex++;
-                        }else if(curVal<-1){
-                            this.nowIndex--;
-                        }
-                    }.bind(this),10)
-                }
-            }
-
-        },
-        mounted(){
-            this.runInv()
-        }
+export default {
+  props: {
+    slides: {
+      type: Array,
+      default: []
+    },
+    inv: {
+      type: Number,
+      default: 1000
     }
+  },
+  data() {
+    return {
+      imgWidth: 900,
+      nowIndex: 0,
+      invId: null,
+      isShow: true,
+      slideReset: false,
+      throttleId: null,
+      delay: 1200,
+      reSetFlag: false,
+      reSetId: null
+    };
+  },
+  computed: {
+    slideIndex() {
+      if (this.nowIndex <= -1) {
+        return this.slides.length - 1;
+      } else if (this.nowIndex >= this.slides.length) {
+        return 0;
+      } else {
+        return this.nowIndex;
+      }
+    }
+  },
+  methods: {
+    prev() {
+      this.nowIndex--;
+    },
+    next() {
+      this.nowIndex++;
+    },
+    goTo(index) {
+      this.nowIndex = index;
+    },
+    throttle(method, arg) {
+      if (this.throttleId) {
+        return;
+      }
+      method(arg);
+      this.throttleId = setTimeout(() => {
+        this.throttleId = undefined;
+      }, this.delay);
+    },
+    runInv() {
+      this.invId = setInterval(() => {
+        this.nowIndex++;
+      }, this.inv);
+    },
+    clearInv() {
+      clearInterval(this.invId);
+      this.invId = null;
+    }
+  },
+  watch: {
+    nowIndex(curVal) {
+      if (curVal === this.slides.length || curVal === -1) {
+        setTimeout(() => {
+          this.slideReset = true;
+          if (curVal === this.slides.length) {
+            this.nowIndex = 0;
+          } else if (curVal === -1) {
+            this.nowIndex = this.slides.length - 1;
+          }
+          this.reSetFlag = true;
+        }, 1000);
+        this.reSetId = setInterval(()=>{
+            if(this.reSetFlag){
+                this.slideReset = false;
+                clearInterval(this.reSetId)
+                this.reSetId=null
+            }
+        },1000)
+      }
+    }
+  },
+  mounted() {
+    this.runInv();
+  }
+};
 </script>
 
 <style scoped>
+.slide-show {
+  position: relative;
+  margin: 15px 15px 15px 0;
+  width: 900px;
+  height: 500px;
+  overflow: hidden;
+}
 
-    .slide-show {
-        position: relative;
-        margin: 15px 15px 15px 0;
-        width: 900px;
-        height: 500px;
-        overflow: hidden;
-    }
+.slide-show h2 {
+  position: absolute;
+  width: 100%;
+  color: #fff;
+  background: #000;
+  opacity: 0.5;
+  bottom: 0;
+  height: 30px;
+  line-height: 30px;
+  text-align: left;
+  padding-left: 15px;
+}
 
-    .slide-show h2 {
-        position: absolute;
-        width: 100%;
-        color: #fff;
-        background: #000;
-        opacity: .5;
-        bottom: 0;
-        height: 30px;
-        line-height: 30px;
-        text-align: left;
-        padding-left: 15px;
-    }
+.slide-img {
+  width: 100%;
+  overflow: hidden;
+}
 
-    .slide-img {
-        width: 100%;
-        overflow: hidden;
-    }
+.slide-img > ul {
+  position: absolute;
+  /*height: 345px;*/
+  width: 1000%;
+  left: 0px;
+}
 
-    .slide-img > ul {
-        position: absolute;
-        /*height: 345px;*/
-        width: 1000%;
-        left: 0px;
-    }
+.slideTransition {
+  transition: left 1s ease;
+}
 
-    .slideTransition {
-        transition: left 1s ease;
-    }
+.slide-img > ul > li {
+  float: left;
+}
 
-    .slide-img > ul > li {
-        float: left;
-    }
+.slide-pages {
+  position: absolute;
+  bottom: 10px;
+  right: 15px;
+}
 
-    .slide-pages {
-        position: absolute;
-        bottom: 10px;
-        right: 15px;
-    }
+.slide-pages li {
+  display: inline-block;
+  padding: 0 10px;
+  cursor: pointer;
+  color: #fff;
+}
 
-    .slide-pages li {
-        display: inline-block;
-        padding: 0 10px;
-        cursor: pointer;
-        color: #fff;
-    }
-
-    .slide-pages li .on {
-        text-decoration: underline;
-        color: lightblue;
-    }
+.slide-pages li .on {
+  text-decoration: underline;
+  color: lightblue;
+}
 </style>
